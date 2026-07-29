15af89ab49d89711b86e58e4d9fb28886787ca46
Mclorenze <78360225+Mclorl@users.noreply.github.com>
2026-07-29 17:38:16 +0800

    added a jump buffer feature
    Addded a jump buffer feature so that it will feel natural whenever the player press the jump key even if they are not on the ground yet.

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -6,6 +6,10 @@ const WALK_SPEED: float = 2.0
 const SPRINT_SPEED: float = 4.0
 const JUMP_VELOCITY: float = 3.5
 
+@export var jump_buffer_timer: float = 0.2
+
+var jump_buffer: bool = false
+var jump_available: bool = true
 var speed = WALK_SPEED
 
 @onready var camera_controller_anchor: Marker3D = $Camera_Controller_Anchor
@@ -16,10 +20,19 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jump_available = true
+		if jump_buffer:
+			jump()
+			jump_buffer = false
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if jump_available:
+			jump()
+		else:
+			jump_buffer = true
+			get_tree().create_timer(jump_buffer_timer).timeout.connect(on_jump_buffer_timeout)
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -44,7 +57,14 @@ func _physics_process(delta: float) -> void:
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 	
 	move_and_slide()
-	
+
+func jump() -> void:
+	velocity.y = JUMP_VELOCITY
+	jump_available = false
+
+func on_jump_buffer_timeout() -> void:
+	jump_buffer = false
+
 	# Note:
 	# - turn on physics interpolation.
 	# - set the physics jitter fix to 0.
